001424e75d2f55a98a1153461e2db2879a74ad41
Elmira Mustafina <emustafi@c1r8s4.42prague.com>
2025-06-23 17:15:11 +0200

    fixed  putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emustafi <elmiramust2010@gmail.com>        +#+  +:+       +#+        */
+/*   By: emustafi <emustafi@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 23:03:56 by emustafi          #+#    #+#             */
-/*   Updated: 2025/06/16 23:08:15 by emustafi         ###   ########.fr       */
+/*   Updated: 2025/06/23 17:06:42 by emustafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
+	char	buf[12];
+	int		i;
+	long	num;
 
-	if (n == INT_MIN)
+	num = n;
+	i = 0;
+	if (num == 0)
 	{
-		write(fd, "-2147483648", 11);
+		write(fd, "0", 1);
 		return ;
 	}
-	if (n < 0)
+	if (num < 0)
 	{
 		write(fd, "-", 1);
-		n = -n;
+		num = -num;
 	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	c = '0' + (n % 10);
-	write(fd, &c, 1);
+	while (num > 0)
+	{
+		buf[i++] = '0' + (num % 10);
+		num /= 10;
+	}
+	while (i > 0)
+		write(fd, &buf[--i], 1);
 }
